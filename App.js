aa99fc99c307917344d050b06b123c0f9872fbda
Ira Jane Renoblas <irajanerenoblas067@gmail.com>
2023-11-19 19:29:39 +0800

    Initial Change

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import data from './src/data';
+import { ScrollView } from 'react-native-web';
+export default class App extends Component {
+ 
+  constructor(props) {
+     
+        super(props);
+        this.state = {
+          tableHead: ['Id', 'Username', 'Password', 'UserType'],
+          tableHead2: ['Id',  'Lastname', 'Firstname', 'Course'],
+          tableHead3: ['Id', 'Name', 'Course'],
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+        }
+
+  }
+  
+
+  render() {
+    const students = data.filter((user) => user.type === "Student");
+    const Item = ({course, id, name}) => (
+      
+      <Rows data={[[id, `${name.fname} ${name.lname} `, course]]}  borderStyle={{borderWidth: 1, borderColor: '#E7B10A' }} style={{borderBottomWidth: 1, borderColor: '#E7B10A', borderRightWidth: 1, borderWidth: 1}} textStyle={styles.text}/>
+     
+    );
+
+    const state = this.state;
+    return (
+      <View style={styles.container}>
+
+      <Text style={{fontSize: 30, fontWeight: '700', fontFamily: 'Times New Roman', color: '#004225'}}>List of Accounts</Text>
+              <Table borderStyle={{borderWidth: 1, borderColor: '#E7B10A'}}>
+
+                <Row data={state.tableHead} style={styles.head} textStyle={{margin: 6, color: 'Black', fontFamily: 'Tahoma', textAlign: 'center'}}/>
+                
+                {data.map((users)=>{
+                  return(
+                    <View>
+                      <Rows data={[[users.id, users.user, users.pass, users.type]]} borderStyle={{borderWidth: 1, borderColor: '#E7B10A' }} style={{borderBottomWidth: 1, borderColor: '#E7B10A', borderRightWidth: 1, borderWidth: 1}} textStyle={styles.text}/>
+                    </View>
+                  )
+                })}
+              </Table>
+
+      <Text style={{fontSize: 30, fontFamily: 'Times New Roman', fontWeight: '700', color: '#004225', marginTop: 20}}>List of Users</Text>
+        <Table borderStyle={{borderWidth: 1, borderColor: '#E7B10A'}}>
+          <ScrollView>
+              <Row data={state.tableHead2}  style={styles.head} textStyle={{margin: 6, color: 'Black', fontFamily: 'Tahoma', textAlign: 'center'}}/>
+              
+              {data.map((users)=>{
+                return(
+                  <View>
+                    <Rows data={[[users.id, users.name.lname, users.name.fname, users.course]]} borderStyle={{borderWidth: 1, borderColor: '#E7B10A' }} style={{borderBottomWidth: 1, borderColor: '#FFB000', borderRightWidth: 1, borderWidth: 1}} textStyle={styles.text}/>
+                  </View>
+                )
+              })}
+          </ScrollView>
+
+        </Table>
+
+        <Text style={{fontSize: 30, fontWeight: '700', fontFamily: 'Times New Roman', color: '#004225', marginTop: 20}}>List of Students</Text>
+        <Table borderStyle={{borderWidth: 1, borderColor: '#E7B10A'}}>
+              <Row data={state.tableHead3}  style={styles.head} textStyle={{margin: 6, color: 'Black', fontFamily: 'Tahoma', textAlign: 'center'}}/>
+              
+              <FlatList
+              data={students}
+              renderItem={({item}) => <Item course={item.course } id={item.id } name={item.name} />}
+              
+              keyExtractor={item => item.id}
+            />
+        
+        </Table>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  container: { flex: 1, padding: 16, paddingTop: 35, backgroundColor: '#FAF0E4' },
+  head: { height: 40, backgroundColor: '#E7B10A' },
+  text: { margin: 6, textAlign: 'center', fontFamily: 'Tahoma', fontSize: 15}
 });
---
+++ src/data.js
@@ -0,0 +1,177 @@
+
+  
+    const DATA = [
+      {
+        id: 0,
+        name: { 
+          fname: "Yamada",
+          lname: "Akito"
+        },
+        nick: "Yamada",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "yamada",
+        pass: "akitoyamada",
+        type: "Student",
+    },
+    {
+        id: 1,
+        name: { 
+          fname: "Yano",
+          lname: "Akiko"
+        },
+        nick: "Yan",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "yanoakiko",
+        pass: "yano123",
+        type: "Student",
+    },
+    {
+        id: 2,
+        name: { 
+          fname: "Jonas",
+          lname: "Frankie"
+        },
+        nick: "Jones",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "jonesfranc",
+        pass: "jf123",
+        type: "Student",
+    },
+    {
+        id: 3,
+        name: { 
+          fname: "Sophie",
+          lname: "Hatter"
+        },
+        nick: "Sophie",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "softphieeee",
+        pass: "sophi3",
+        type: "Student",
+    },
+    {
+        id: 4,
+        name: { 
+          fname: "Akane",
+          lname: "Kinoshita"
+        },
+        nick: "Akane",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "akan3",
+        pass: "4k4n3onetwo",
+        type: "Student",
+    },
+    {
+        id: 5,
+        name: { 
+          fname: "Kyoo",
+          lname: "Kyuko"
+        },
+        nick: "Kyo",
+        course: "BS Information Technology",
+        yrsec: "3B",
+        user: "kyoyooo",
+        pass: "kyusky0",
+        type: "Student",
+    },
+    {
+      id: 6,
+      name: { 
+        fname: "Earl Mike",
+        lname: "Sarabia"
+      },
+      nick: "Andyed",
+      course: "BS Information Technology",
+      yrsec: "3A",
+      user: "mike03",
+      pass: "mikekuzmikekuz",
+      type: "Student",
+    },
+    {
+      id: 7,
+      name: { 
+        fname: "Ira Jane",
+        lname: "Renoblas"
+      },
+      nick: "Ajane",
+      course: "BS Information Technology",
+      yrsec: "3B",
+      user: "irajane03",
+      pass: "er4jani3",
+      type: "Student",
+    },
+    {
+      id: 8,
+name: { 
+      fname: "Winzeal",
+      lname: "Agoc"
+    },
+    nick: "Winzeal",
+    course: "",
+    yrsec: "",
+    user: "winzeal08",
+    pass: "winzeal123",
+    type: "Teacher",
+  },
+  {
+      id: 9,
+ name: { 
+        fname: "Jerry",
+        lname: "Ale"
+      },
+      nick: "Sirzer",
+      course: "",
+      yrsec: "",
+      user: "jerryy",
+      pass: "jerry123",
+      type: "Teacher",
+  },
+  {
+    id: 10,
+name: { 
+        fname: "Fermo",
+        lname: "Asufra"
+      },
+      nick: "Bords",
+      course: "",
+      yrsec: "",
+      user: "bords12",
+      pass: "fermo123",
+      type: "Teacher",
+  },
+  {
+      id: 11,
+      name: { 
+      fname: "Gabrene",
+      lname: "Diaz"
+    },
+    nick: "Sir D",
+    course: "",
+    yrsec: "",
+    user: "gabrene09",
+    pass: "gabrene123",
+    type: "Teacher",
+  },
+  {
+    id: 12,
+    name: { 
+      fname: "Catherine Leah",
+      lname: "Gabo"
+    },
+    nick: "Leah",
+    course: "",
+    yrsec: "",
+    user: "cathleah",
+    pass: "leah123",
+    type: "Teacher",
+  },
+    ];
+  
+    export default DATA;
+  
+
